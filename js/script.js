b95e8af364c506d93c5883f41fe5562416467bfa
kkarakhanian <karakhanian.k@gmail.com>
2025-05-26 17:25:26 +0300

    Add script

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let userInput=prompt("What is your name?");
+if (userInput === "John") {
+    alert("Hello " + userInput + "! How are you?");
+}
+else {
+    alert("Hello " + userInput + "! Happy to see you");
+}
