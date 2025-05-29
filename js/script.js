d081b9e6aab1f40080bb8015449d19de5bae97af
kkarakhanian <karakhanian.k@gmail.com>
2025-05-29 16:25:50 +0300

    Add new script for homework

--- js/script.js
+++ js/script.js
@@ -1,23 +1,38 @@
 "use strict";
 
-const UserName = "Karen" // This const indicates that the user's name is Karen.
-console.log(typeof UserName)
-let UserAge = 27 // This variable indicates that the user is 27 years old
-console.log(typeof UserAge)
-const isUserVerified = true // This const indicates that the user is verified in the system
-console.log(typeof isUserVerified)
-const registrationDate = new Date(2024, 8, 17) // This const indicates that the user  is registered on the specified date
-console.log(typeof registrationDate)
-let UserCardNumber = BigInt(4627051318145941) // This const indicates that user has oun card number. It could be changed after a while
-console.log(typeof UserCardNumber)
-let UserPromo = "SPRING2025"// This const indicates user's promocode
-console.log(typeof UserPromo)
-let UserReserveName; // This variable indicates that the user does not have a reserve name at the moment, but that space for it has been reserved for the future.
-console.log(typeof UserReserveName)
-let UserBonuses = 351 // This variable indicates user's bonuses. They may change values in the course of use
-console.log(typeof UserBonuses)
+let UserName;
+UserName=prompt('What is your name?');
+console.log(typeof UserName);
+console.log(`Username: ${UserName}`);
 
+let Age;
+Age = prompt('Write your age');
+console.log(typeof Number(Age));
+console.log(`Age: ${Age}`)
 
-console.log(`User ${UserName}, Age ${UserAge}, Verified: ${isUserVerified}`)
-console.log(`Registration Date: ${registrationDate}, Bonuses: ${UserBonuses} , Promo: ${UserPromo}`)
-console.log(`User: ${UserName}, Reserve name: ${UserReserveName}, Age: ${UserAge}`)
+let UserAcceptance;
+UserAcceptance = confirm('Do you want to take part in our promo');
+console.log(typeof UserAcceptance);
+console.log(UserAcceptance);
+if (UserAcceptance) {
+    alert('Thanks for your help')
+} else {
+    alert('You chosen No')
+}
+
+const UserId = 1895043789053n;
+let BonusCode = null;
+let SecondName;
+
+alert(
+    `Hello: ${UserName}!
+Your age: ${Age}
+Your status ${UserAcceptance}`
+)
+
+console.log ("Name", UserName, typeof UserName);
+console.log("Age", Age, typeof Number(Age));
+console.log("User status", UserAcceptance, typeof UserAcceptance);
+console.log("User ID", UserId, typeof UserId);
+console.log("Bonus Code", BonusCode, typeof BonusCode);
+console.log("SecondName", SecondName, typeof SecondName);
