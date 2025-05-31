d1bc77ed79510405e017df4bcc053c7d93d01d59
kkarakhanian <karakhanian.k@gmail.com>
2025-05-31 16:18:58 +0300

    add new branch with homework

--- js/script.js
+++ js/script.js
@@ -1,22 +1,45 @@
 "use strict";
 
-const user = {
-    name: 'Paul',
-    age: 30,
-    isOnline: false,
-    friends: ['Karolina', 'Maxim', 'Dany'],
-    settings: {
-        theme: 'dark',
-        notifications: true
-    }
-}
-const firstFriendName = user.friends[0];
-let notificationsEnabled = user.settings.notifications;
+let name;
+name=prompt('What is your name?');
 
-console.log(
-    user.name,
-    user.friends.length,
-    firstFriendName,
-    user.settings.theme,
-    notificationsEnabled
-    )
+let age;
+age=prompt('How old are you?')
+age=Number(age);
+
+let notifications;
+notifications=confirm('Do you want to get notifications?')
+
+const userId=9780652917n;
+let lastLogin=null;
+let nickname;
+
+let favoriteTech=['JavaScript','Python', 'React'];
+
+let settings= {
+    theme: 'light',
+    autoLogin: false,
+}
+
+let session={
+    name: name,
+    age: age,
+    notifications: notifications,
+    userId: userId,
+    lastLogin: lastLogin,
+    nickname: nickname,
+    favoriteTech: favoriteTech,
+    settings: settings,
+}
+
+alert(`Hello, ${name}! Your id: [${userId}]`);
+console.log(session);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof notifications);
+console.log(typeof userId);
+console.log(typeof lastLogin);
+console.log(typeof nickname);
+console.log(typeof favoriteTech);
+console.log(typeof settings);
