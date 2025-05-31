658dd01a25eb1b5e9f4c9e6cd9f12e5168356bd7
kkarakhanian <karakhanian.k@gmail.com>
2025-05-31 10:44:45 +0300

    Add new homework with script

--- js/script.js
+++ js/script.js
@@ -1,38 +1,22 @@
 "use strict";
 
-let userName;
-userName=prompt('What is your name?');
-console.log(typeof userName);
-console.log(`Username: ${userName}`);
-
-let age;
-age = prompt('Write your age');
-console.log(typeof Number(age));
-console.log(`Age: ${age}`)
-
-let userAcceptance;
-userAcceptance = confirm('Do you want to take part in our promo');
-console.log(typeof userAcceptance);
-console.log(userAcceptance);
-if (userAcceptance) {
-    alert('Thanks for your help')
-} else {
-    alert('You chosen No')
+const user = {
+    name: 'Paul',
+    age: 30,
+    isOnline: false,
+    friends: ['Karolina', 'Maxim', 'Dany'],
+    settings: {
+        theme: 'dark',
+        notifications: true
+    }
 }
+const firstFriendName = user.friends[0];
+let notificationsEnabled = user.settings.notifications;
 
-const userId = 1895043789053n;
-let bonusCode = null;
-let secondName;
-
-alert(
-    `Hello: ${userName}!
-Your age: ${age}
-Your status ${userAcceptance}`
-)
-
-console.log ("Name", userName, typeof userName);
-console.log("Age", age, typeof Number(age));
-console.log("User status", userAcceptance, typeof userAcceptance);
-console.log("User ID", userId, typeof userId);
-console.log("Bonus Code", bonusCode, typeof bonusCode);
-console.log("SecondName", secondName, typeof secondName);
+console.log(
+    user.name,
+    user.friends.length,
+    firstFriendName,
+    user.settings.theme,
+    notificationsEnabled
+    )
