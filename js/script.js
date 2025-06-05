ecb0f36c18f9e305b23eb5395d691677607ad51f
kkarakhanian <karakhanian.k@gmail.com>
2025-06-05 09:18:27 +0300

    Create new branch with hw

--- js/script.js
+++ js/script.js
@@ -1,41 +1,10 @@
 "use strict";
 
-let name=prompt('What is your name?');
+let num1 = +prompt('Please enter the first number');
+let num2 = +prompt('Please enter the second number');
+let num3 = +prompt('Please enter the third number');
 
-let age=Number(prompt('How old are you?'))
+let sum = num1 + num2 + num3;
+let average = sum / 3;
 
-let notifications=confirm('Do you want to get notifications?')
-
-const userId=9780652917n;
-let lastLogin=null;
-let nickname;
-
-let favoriteTech=['JavaScript','Python', 'React'];
-
-let settings= {
-    theme: 'light',
-    autoLogin: false,
-}
-
-let session={
-    name: name,
-    age: age,
-    notifications: notifications,
-    userId: userId,
-    lastLogin: lastLogin,
-    nickname: nickname,
-    favoriteTech: favoriteTech,
-    settings: settings,
-}
-
-alert(`Hello, ${name}! Your id: [${userId}]`);
-console.log(session);
-
-console.log(typeof name);
-console.log(typeof age);
-console.log(typeof notifications);
-console.log(typeof userId);
-console.log(typeof lastLogin);
-console.log(typeof nickname);
-console.log(typeof favoriteTech);
-console.log(typeof settings);
+alert('Your arithmetic mean: ' + average);
