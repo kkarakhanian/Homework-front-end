17d35848fba7cd8aaa422a3cd317ec14b96b4c54
kkarakhanian <karakhanian.k@gmail.com>
2025-06-08 17:27:20 +0300

    Create new branch with hw

--- js/script.js
+++ js/script.js
@@ -1,117 +1,68 @@
 'use strict';
 
-//Print on the page in one line separated by commas the numbers from 10 to 20.
+//Print the numbers from 20 to 30, separated by a space, using a step of 0.5 (20 20.5 21 21.5....).
 let output = '';
-for (let i = 10; i <= 20; i++) {
+for (let i = 20; i <= 30; i += 0.5) {
     output += i;
-    if (i < 20) {
+    if (i <= 30) {
         output += ', ';
+}
     }
-}
-    console.log(output);
+console.log(output);
 
-//Print the squares of numbers from 10 to 20.
-let result = '';
-for (let i = 10; i <= 20; i++) {
-    const square = i * i;
-    result += square;
-    if (i < 20) {
-        result += ', ';
-    }
+//One dollar is worth 27 UAH. Print the data with the cost of 10, 20, 30 ... 100 dollars.
+let cours = 27;
+for (let i = 10; i <= 100; i += 10) {
+    const cost = i * cours;
+    console.log(`${i} USD = ${cost} UAH`);
 }
-console.log(result);
 
-//Print the table of multiplication by 7.
-let tab = '';
-for (let i = 1; i <= 10; i++) {
-    const result = 7 * i;
-    tab += `7 * ${i} = ${result}`;
-    if (i < 10) {
-        tab += '\n';
-    }
+//An integer is given. Print all integers from 1 to 100, the square of which does not exceed the number N.
+let integer = 10;
+for (let i = 1; i <= 100; i++) {
+     const squareOf = i * i;
+if (squareOf <= 10) {
+    console.log(i);
+} else {
+    break;
 }
-console.log(tab);
-
-//Find the sum of all integers from 1 to 15.
-let sum = 0;
-for (let i = 1; i <= 15; i++) {
-    sum += i;
 }
-console.log(+sum)
 
-//Find the product of all integers from 15 to 35.
-let dob = 1n;
-for (let i = 15; i <= 35; i++) {
-    dob *= BigInt(i);
-}
-console.log(dob);
-
-//Find the arithmetic mean of all integers from 1 to 500.
-let sum1 = 0;
-for (let i = 1; i <= 500; i++) {
-    sum1 += i;
-    const count = 500;
-    const average = sum1 / count;
-}
-console.log(sum1);
-
-//Print the sum of only even numbers in the range from 30 to 80.
-let sumOfEvens = 0;
-for(let i = 30; i <= 80; i++) {
-    if (i % 2 === 0) {
-        sumOfEvens += i;
-    }
-}
-    console.log(sumOfEvens)
-
-//Print all numbers in the range from 100 to 200 multiples of 3.
-let finalResult = [];
-for (let i = 100; i <= 200; i++) {
-    if (i % 3 === 0) {
-        finalResult += i;
-        finalResult += ', ';
-    }
-}
-console.log(finalResult);
-
-//A positive integer is given. Find and print all its divisors.
-let divisors = [];
-for (let i=1; i <= 8; i++) {
-    if (8 % i === 0) {
-        divisors += i;
-        divisors += ', ';
-    }
-}
-console.log(divisors);
-
-//Find the number of its even divisors.
-let divisors1 = [];
-for (let i=1; i <= 8; i++) {
-    if (8 % i === 0) {
-        if (i % 2 === 0) {
-            divisors1 += i;
-            divisors1 += ', ';
+//An integer is given. Find out if it is prime (a prime is a number greater than 1 that has no divisors other than 1 and itself).
+let integral1 = 2;
+let isPrime = true;
+if (integral1 <= 1) {
+    isPrime = false;
+} else {
+    for (let i = 2; i <= Math.sqrt(integral1); i++) {
+        if (integral1 % i === 0) {
+            isPrime = false;
+            break;
         }
     }
 }
-console.log(divisors1);
 
-//Find the sum of its even divisors.
-let sumOfDivisors = 0;
-for (let i=1; i <= 8; i++) {
-    if (8 % i === 0) {
-        if (i % 2 === 0) {
-           sumOfDivisors += i;
-        }
+if (isPrime) {
+    console.log(integral1 + " — is a prime number.");
+} else {
+    console.log(integral1 + " — is not a prime number.");
+}
+
+//Some number is given. Determine whether this number can be obtained by raising the number 3 to a certain power.
+// (For example, the numbers 9, 81 can be obtained, but 13 cannot.)
+let givenNumber = 15;
+let isPowerOfThree = false;
+let power = 1;
+
+for (let power = 1; power <= 15; power *= 3) {
+    if (power === 15) {
+        isPowerOfThree = true;
+        break;
     }
 }
-console.log(sumOfDivisors);
 
-//Print the complete multiplication table from 1 to 10.
-for (let i = 1; i <= 10; i++) {
-    console.log(`--- Таблиця множення на ${i} ---`);
-    for (let j = 1; j <= 10; j++) {
-        console.log(`${i} x ${j} = ${i * j}`);
-    }
-    console.log('\n');
+if (isPowerOfThree) {
+    console.log(15 + " can be obtained as 3 to some degree.");
+} else {
+    console.log(15 + " can't be obtained as 3 to some degree..");
 }
